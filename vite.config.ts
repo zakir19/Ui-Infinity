449b02c9df7707954264595690bda1391d19542c
zakir19 <patelzakir784@gmail.com>
2025-09-05 09:24:58 +0530

    changes for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,24 +3,14 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-  ],
+export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
   build: {
-    outDir: "dist", 
-    sourcemap: true, 
-    rollupOptions: {
-      external: [],
-    },
+    outDir: "dist",
   },
-}));
+});
