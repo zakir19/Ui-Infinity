6c10fdfb3cc4d0cfdb8cbe291b0cc7548b00cb9c
zakir19 <patelzakir784@gmail.com>
2025-09-06 18:28:58 +0530

    23

--- build-test.js
+++ build-test.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-const { execSync } = require('child_process');
-const fs = require('fs');
+import { execSync } from 'child_process';
+import fs from 'fs';
 
 console.log('🔨 Testing build process...\n');
 
